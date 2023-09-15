dd55dcd5f3bed6bc3c416d56bb129e265072b350
Alekasnder Giera <aleksander.giera@tietoevry.com>
2023-09-15 16:33:13 +0200

    added markdown preview

--- lua/alegie/packer.lua
+++ lua/alegie/packer.lua
@@ -30,4 +30,11 @@ return require('packer').startup(function(use)
 	use({ 'ThePrimeagen/harpoon' })
 
 	use({ 'mbbill/undotree' })
+
+	use({ 
+		"iamcco/markdown-preview.nvim", 
+		run = "cd app && npm install", 
+		setup = function() vim.g.mkdp_filetypes = { "markdown" } end, 
+		ft = { "markdown" }, 
+	})
 end)
