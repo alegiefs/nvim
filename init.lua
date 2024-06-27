513ad2b68a761bbe8e0499fe694789a16c207276
Aleksander Giera <aleksander.giera@tietoevry.com>
2024-06-27 10:21:33 +0200

    add formatting for xml

--- init.lua
+++ init.lua
@@ -4,6 +4,16 @@ vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.clipboard="unnamed,unnamedplus"
 
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+
 -- quickscope
 vim.g.qs_highlight_on_keys={'f', 'F', 't', 'T'}
 -- quickscope
+
+
+-- formatting options
+vim.api.nvim_exec([[
+	au FileType xml setlocal equalprg=xmllint\ --format\ --recover\ -\ 2>/dev/null
+]], false)
